c3d1396b6ccf6a73b72e9542b93e506828114c6d
Gauraveet Singh <gauravjeet.singh@shipsy.io>
2021-10-22 14:18:36 +0530

    added submodule1

--- app/src/main/java/com/example/parentapp/MainActivity.kt
+++ app/src/main/java/com/example/parentapp/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
+   //Change in App Module
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
